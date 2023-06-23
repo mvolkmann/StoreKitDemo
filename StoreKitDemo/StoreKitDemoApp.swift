8aeef69a92478a1b727c48f4f9a44c44ccc7c0d6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-23 12:32:46 -0500

    Initial Commit

---
+++ StoreKitDemo/StoreKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKitDemoApp.swift
+//  StoreKitDemo
+//
+//  Created by Mark Volkmann on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct StoreKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
