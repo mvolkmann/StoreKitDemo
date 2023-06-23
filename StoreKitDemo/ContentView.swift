09129b60b2ce2839d45cac3ed20f07471f01cd8a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-23 14:46:50 -0500

    progress

--- StoreKitDemo/ContentView.swift
+++ StoreKitDemo/ContentView.swift
@@ -13,14 +13,17 @@ private let icons: [String: Image] = [
 ]
 
 struct ContentView: View {
+    @Environment(\.dismiss) var dismiss
     @EnvironmentObject private var store: StoreKitStore
+    @State private var isPurchasing = false
 
     // TODO: Can you get the product ids from the store object?
-    let productIds = ["p1", "p2", "p3"]
+    private let productIds = ["p1", "p2", "p3"]
+    private let iconSize = 70.0
 
     private func productIcon(id: String) -> some View {
         ZStack {
-            Circle().fill(.gray).opacity(0.5)
+            Circle().fill(.gray.opacity(0.2))
             let color: Color = colors[id] ?? .black
             let image = icons[id] ??
                 Image(systemName: "circle.slash")
@@ -28,21 +31,72 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(color)
-                .frame(width: 40)
+                .frame(width: iconSize / 2)
         }
-        .frame(width: 70)
+        .frame(width: iconSize, height: iconSize)
     }
 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        VStack(alignment: /*@START_MENU_TOKEN@*/ .center/*@END_MENU_TOKEN@*/) {
+            Text("Choose a plan.").fontWeight(.bold)
+
             StoreView(ids: productIds) { product in
-                productIcon(id: product.id)
+                // productIcon(id: product.id)
+                ProductView(id: product.id) {
+                    productIcon(id: product.id)
+                } placeholderIcon: {
+                    Circle()
+                }
             }
+            // This size has an issue with horizontal alignment.
+            // .productViewStyle(.large)
+            // .productViewStyle(.regular) // default
+            .productViewStyle(.compact)
+
+            // TODO: Why doesn't this work?
+            /*
+             SubscriptionStoreView(groupID: "subscriptions")
+                 .containerBackground(for: .subscriptionStoreFullHeight) {
+                     Color.yellow
+                 }
+                .subscriptionStoreButtonLabel(.multiline)
+                .subscriptionStorePickerItemBackground(.thinMaterial)
+                .storeButton(.visible, for: .redeemCode)
+             */
         }
         .padding()
+        .background(.yellow.opacity(0.3))
+
+        // This handles the event from any descendant view.
+        .onInAppPurchaseStart { _ in
+            isPurchasing = true
+            // This might be used to dim other views.
+        }
+
+        // This handles the event from any descendant view.
+        .onInAppPurchaseCompletion { product, result in
+            if case let .success(.success(transaction)) = result {
+                print("product =", product)
+                print("transaction =", transaction)
+                // TODO: process the transaction here
+                dismiss()
+            }
+            isPurchasing = false
+        }
+
+        .subscriptionStatusTask(for: "subscriptions") { taskState in
+            // TODO: Can you use this to avoid showing purchase options
+            // TODO: to users that have already purchased one of them?
+            print("taskState =", taskState)
+        }
+
+        .currentEntitlementTask(
+            for: "r.mark.volkmann.gmail.com.StoreKitDemo"
+        ) { state in
+            // An enum value in the state object can have the values
+            // loading, failure(any Error), or success(Entitlement).
+            print("state =", state)
+        }
     }
 }
 
